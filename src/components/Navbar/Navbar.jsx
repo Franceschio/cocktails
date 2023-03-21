4624dfd0d0798df216a75052d8e662db6bb3954d
Franceschio <coppolaciccio1@gmail.com>
2023-03-21 23:11:28 +0100

    Cocktails page project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,6 +9,11 @@ const Navbar = () => {
   return (
     <div className={`Navbar`}>
       <ul>
+        <li className="hamburgerMenu">
+          <div className="hamburgerLine"></div>
+          <div className="hamburgerLine"></div>
+          <div className="hamburgerLine"></div>
+        </li>
         <a href="#">
           <li>Home</li>
         </a>
@@ -19,6 +24,10 @@ const Navbar = () => {
           <li>Contact</li>
         </a>
       </ul>
+      <form className="searchBar">
+        <input className="search" type="text" placeholder="Search..." />
+        <input className="searchBtn" type="submit" value="search" />
+      </form>
       <div className="logo">
         <img
           src="https://img.icons8.com/external-vitaliy-gorbachev-blue-vitaly-gorbachev/512/external-cocktail-vacation-vitaliy-gorbachev-blue-vitaly-gorbachev.png"
